dcffa5db70c88c8220ff745422953d290e8cbcd4
sakib biswas <sakibbiswas418@gmail.com>
2023-02-04 06:10:23 -0800

    skills ui done

--- src/conponents/Skills.js
+++ src/conponents/Skills.js
@@ -1,7 +1,57 @@
-import React from "react";
+import SectionTitle from "./SectionTitle";
+const data = [
+  { id: 1, title: "HTML" },
+  { id: 2, title: "CSS" },
+  { id: 3, title: "Bootstrap" },
+  { id: 4, title: "Tailwind CSS" },
+  { id: 5, title: "JavaScript" },
+  { id: 6, title: "React.js" },
+  { id: 7, title: "React Router" },
+  { id: 8, title: "Redux.js" },
+  { id: 9, title: "Redux Toolkit" },
+  { id: 10, title: "Axios" },
+  { id: 11, title: "GSAP" },
+  { id: 12, title: "Firebase" },
+];
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="skills container mx-auto mt-40" id="skills">
+      <SectionTitle title={"skills"} />
+
+      <div className="skills-wrapper grid grid-cols-2 mt-40 gap-20">
+        <ul className="skills-left flex flex-col gap-10 ">
+          {data
+            .filter((_, i) => i < Math.floor(data.length / 2))
+            .map((skill, i) => (
+              <li key={skill.id}>
+                <div className="skill-item flex gap-10 items-baseline">
+                  <span className="skill-num text-white/50 text-7xl">
+                    {String(skill.id).padStart(2, 0).padEnd(3, ".")}
+                  </span>
+                  <span className="skill-name text-[2rem]">{skill.title}</span>
+                </div>
+              </li>
+            ))}
+        </ul>
+
+        <ul className="skills-right flex flex-col gap-10">
+          {data
+            .filter((_, i) => i >= Math.floor(data.length / 2))
+            .map((skill, i) => (
+              <li key={skill.id}>
+                <div className="skill-item flex gap-10 items-baseline">
+                  <span className="skill-num text-white/50 text-7xl">
+                    {String(skill.id).padStart(2, 0).padEnd(3, ".")}
+                  </span>
+                  <span className="skill-name text-[2rem]">{skill.title}</span>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
